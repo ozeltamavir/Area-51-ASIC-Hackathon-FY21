8ad6ac4c2de0f888ca4f53a62fcf4050eb4401c4
urosmiha <mihajlovic.uros.96@gmail.com>
2021-09-13 09:47:08 +1200

    Front end changes
    Only run API when button is clicked

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
 
-  getURL()
+  // getURL()
+  // FetchDNAC()
 
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
     let url = tabs[0].url;
@@ -8,12 +9,15 @@ window.addEventListener('load', function () {
     // use `url` here inside the callback because it's asynchronous!
   });
 
-  console.log("HEY")
+  // console.log("HEY")
   makeBoxes(24);
 
-  let switchports = document.querySelectorAll(".square");
+  // fetchDNAC();
 
-  console.log(switchports)
+  let switchports = document.querySelectorAll(".square");
+  let save_button = document.querySelectorAll(".fakeButton");
+
+  // console.log(switchports)
   var xhttp = new XMLHttpRequest();
 
   var port = 0;
@@ -22,8 +26,6 @@ window.addEventListener('load', function () {
 
   function setCurrentPort(portnumber) {
     port = portnumber;
-
-
   }
 
   function getCurrentPort() {
@@ -33,35 +35,38 @@ window.addEventListener('load', function () {
   updatePlaceholderInfo();
 
   for (i = 0; i < switchports.length; i++) {
-    console.log(switchports[i])
+    // console.log(switchports[i])
     switchports[i].addEventListener('click', async function (event) {
-      // alert('hey');
-      makeOthersStayTheSame();
-      button = event.target;
-      setCurrentPort(button.id)
-      button.classList.add("blueSquare");
-      updatePlaceholderInfo();
-      // xhttp.open('POST', 'http://localhost:8008/')
-      // xhttp.send()
-      // xhttp.onload = function(){
-      console.log("HEY")
 
-      let b = await httpCall('POST','https://e540-121-74-227-72.ngrok.io', 'Hi:Bye')
-      text = await b.text()
-      alert(text)  
-      
-     
-      // }
+      button = event.target;
+      // alert()
+      if (button.id === '0') {
+        // They clicked on save 
+        alert('Button Clcked');
+      }
+      else {
+        makeOthersStayTheSame();
+        // button = event.target;
+        setCurrentPort(button.id)
+        button.classList.add("blueSquare");
+        updatePlaceholderInfo();
+
+        var body = {
+            "ip" : "10.0.0.20",
+            "interface" : {
+              "name" : "GigabitEthernet",
+              "id" : "1/0/5",
+              "status" : "shut"
+            }
+        }
+        let b = await httpCall('POST','http://localhost:8008/', JSON.stringify(body))
+      }
     })
 
-  }
-
-
-  async function tester(){
-    let b = await httpCall('https://e540-121-74-227-72.ngrok.io', 'Hi:Bye')
-    text = await b.text()
-    alert(text)
-    return b;
+    function saveConfigButton()
+    {
+      alert('Button Clcked');
+    }
 
   }
 
@@ -74,7 +79,7 @@ window.addEventListener('load', function () {
 
   function updatePlaceholderInfo() {
     let portinfoRef = document.querySelector("#port-info");
-    console.log("THIS IS: " + portinfoRef.innerHTML)
+    // console.log("THIS IS: " + portinfoRef.innerHTML)
     portinfoRef.innerHTML = ("This is port no. " + getCurrentPort())
 
   }
@@ -87,9 +92,17 @@ function makeBoxes(Howmany) {
   for (var i = 0; i < Howmany; i++) {
     console.log("Port " + i)
     // document.getElementById("switch-ports").insertAdjacentHTML("afterend", square)
-    id = i + 1
-    square = "<div id='" + id + "' class='square'></div>"
-    document.getElementById("switch-ports").innerHTML += square;
+    if (i % 7 === 6) {
+      id = i + 1
+      console.log("HEREEEE");
+      square = "<div id='" + id + "' class='square' style='margin-left:15px;'></div>"
+      document.getElementById("switch-ports").innerHTML += square;
+    }
+    else { 
+      id = i + 1
+      square = "<div id='" + id + "' class='square'></div>"
+      document.getElementById("switch-ports").innerHTML += square;
+    }
   }
 }
 
@@ -98,21 +111,52 @@ function getURL() {
 }
 
 
-async function httpCall(method, ipaddress, json_body) {
+async function httpCall(method, url, json_body) {
 
-  const response =  fetch(ipaddress, {
-    method: method,
+  (async () => {
+    const rawResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: json_body
+    });
+    const content = await rawResponse.json();
+  
+    console.log(content);
+
+    return content
+  })();
+}
+
+// Get information about the switch from DNAC
+async function fetchDNAC(){
+
+  let url = 'https://198.18.133.101:443/dna/intent/api/v1/interface/network-device/1111bf67-8429-44be-be88-47d6e8b3259a'
+
+  const response = await fetch(url, {
+    method: 'GET',
+    // body: myBody, // string or object
     mode: 'no-cors',
     headers: {
-      'Content-Type': 'application/json'
-    },
-    body: json_body
+      'x-auth-token' : 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZmJmMGZiYWEwZTFjZTAwY2I5OTU3NGQiLCJhdXRoU291cmNlIjoiaW50ZXJuYWwiLCJ0ZW5hbnROYW1lIjoiVE5UMCIsInJvbGVzIjpbIjVmYmYwZmI5YTBlMWNlMDBjYjk5NTc0YyJdLCJ0ZW5hbnRJZCI6IjVmYmYwZmI4YTBlMWNlMDBjYjk5NTc0YSIsImV4cCI6MTYzMTM0MTgxNiwiaWF0IjoxNjMxMzM4MjE2LCJqdGkiOiJiZjg5NmIxZS1kYWIzLTRlM2ItODYzNS1lOTM5MDliZjQxYjgiLCJ1c2VybmFtZSI6ImFkbWluIn0.vsLTh-I39BRzn3Ynw_S5WB1SBB1I_gqbi9un9kDw7UakaycfjcVulY47g9NeeTDHus1eoTAy62qi1E6WNcwSlD5pmFoQVX0I1d_omRLC_HysBONGHRoylxHYGqA9gRdFncEEJiw5jLk9tTK4VFJs3vJZREXOaIbrqK3K7Tl1ZQx4e7UGRIYRUO1x1OsekKUUr5sHp7i90eQu9XFQhl90P04zwrRQD_1m7FLZaBXtD1Xt_8bjnGAFPdVrlXlqoINh3yrceSohj8He8J7AKwbFf2XXWgCx8ZUN4JDX60bSufJ1mSRL-CzIlXWwfZDSso7RhGR8lGn5mvWtFqCnNzKtng',
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    }
+  });
 
-  } );
+  console.log("API Call made here ....");
 
-  // let json_response = response.json()
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
 
-   return await response;
+  // var obj = JSON.parse(myJson.response);
 
+  console.log(myJson.response[0])
 
+  // let mngmt_ip = myJson['46']
+
+  // console.log(mngmt_ip);
+  // console.log("Status: " + response.status);
 }
